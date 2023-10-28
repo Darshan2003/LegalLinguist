41e25354f8d4eac0c2aa98793a6ea1f786c8f2c4
Tejas <jtjtejas05@gmail.com>
2023-10-28 15:14:39 +0530

    search refactor

--- frontend/0_🏠_Home.py
+++ frontend/0_🏠_Home.py
@@ -1,8 +1,7 @@
 import streamlit as st
-import fitz
 from src.Chat import Chat
 from src.utils import page_init
-
+import src.pdfops as pdfops
 
 page_init()
 
@@ -10,39 +9,11 @@ page_init()
 chat = Chat()
 
 
-def search_and_highlight(doc, text):
-    pdf_doc = fitz.open(stream=doc.read(), filetype="pdf")
-    found_something = False
-    for page in pdf_doc:
-        quads = page.search_for(text, quads=True)
-        if quads:
-            found_something = True
-            page.add_highlight_annot(quads)
-            chat.message_by_assistant(
-                page.get_pixmap().tobytes('png'), type='image')
-    if not found_something:
-        chat.message_by_assistant("No matches found.")
-
-
 def processinput(input: str):
     if not input:
         return
     if input.split(' ')[0] == '/search':
-        try:
-            doc_index = int(input.split(' ')[1]) - 1
-            if doc_index >= len(st.session_state['doc']):
-                raise ValueError
-            # search string
-            search_string = ' '.join(input.split(' ')[2:])
-            chat.message_by_assistant(
-                "Searching for " + search_string + " in file " + str(doc_index + 1))
-            search_and_highlight(
-                st.session_state['doc'][doc_index], search_string)
-
-        except ValueError:
-            chat.message_by_assistant(
-                f'You have uploaded only {len(st.session_state["doc"])} files. Please enter a valid index.')
-            return
+        pdfops.search_logic(chat, input)
 
 
 chat.set_processinput(processinput)
---
+++ frontend/src/pdfops.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import fitz
+
+
+def search_and_highlight(chat, doc, text):
+    pdf_doc = fitz.open(stream=doc.read(), filetype="pdf")
+    found_something = False
+    for page in pdf_doc:
+        quads = page.search_for(text, quads=True)
+        if quads:
+            found_something = True
+            page.add_highlight_annot(quads)
+            chat.message_by_assistant(
+                "Found " + str(len(quads)) +
+                " matches in page " + str(page.number) + "."
+            )
+            chat.message_by_assistant(
+                page.get_pixmap().tobytes('png'), type='image')
+    if not found_something:
+        chat.message_by_assistant("No matches found.")
+
+
+def search_logic(chat, input):
+    try:
+        doc_index = int(input.split(' ')[1]) - 1
+        if doc_index >= len(st.session_state['doc']):
+            raise ValueError
+
+        # search string
+        search_string = ' '.join(input.split(' ')[2:])
+        chat.message_by_assistant(
+            "Searching for " + search_string + " in file " + str(doc_index + 1))
+        search_and_highlight(chat,
+                             st.session_state['doc'][doc_index], search_string)
+
+    except ValueError:
+        chat.message_by_assistant(
+            f'You have uploaded only {len(st.session_state["doc"])} files. Please enter a valid index.')
+        return
